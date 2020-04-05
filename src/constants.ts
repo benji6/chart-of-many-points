24e3d0a08dca49c2c5e652ce377147bfa4318cea
Ben Hall <benji2357@gmail.com>
2020-04-05 14:55:47 +0100

    refactor(constants): extract more constants into constants module

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
+export const DISPLAY_DURATION = 2e3;
 export const HEIGHT = innerHeight;
+export const POINT_WIDTH = 2.5;
 export const WIDTH = innerWidth;
 
 const numPixels = WIDTH * HEIGHT;
 const pointsToPixelsRatio = 6e-2;
-
 export const TOTAL_POINTS = Math.round(numPixels * pointsToPixelsRatio);
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import * as screenfull from "screenfull";
 import layouts from "./layouts";
 import { Point } from "./types";
 import { regl } from "./globals";
-import { TOTAL_POINTS, HEIGHT, WIDTH } from "./constants";
+import {
+  DISPLAY_DURATION,
+  HEIGHT,
+  POINT_WIDTH,
+  TOTAL_POINTS,
+  WIDTH,
+} from "./constants";
 import makeDrawPoints from "./makeDrawPoints";
-
-const duration = 2e3;
-const pointWidth = 2.5;
 
 const points: Point[][] = layouts
   .slice(0, 2)
@@ -19,20 +22,17 @@ let newLayoutIndex = 1;
 regl.frame(({ time }) => {
   if (startTime === undefined) startTime = time;
 
-  regl.clear({
-    color: [0, 0, 0, 1],
-    depth: 1,
-  });
+  regl.clear({ color: [0, 0, 0, 1], depth: 1 });
 
   drawPoints({
-    duration,
-    pointWidth,
+    duration: DISPLAY_DURATION,
+    pointWidth: POINT_WIDTH,
     stageHeight: HEIGHT,
     stageWidth: WIDTH,
     startTime,
   });
 
-  if (time - startTime > duration / 1000) {
+  if (time - startTime > DISPLAY_DURATION / 1000) {
     startTime = undefined;
     newLayoutIndex =
       newLayoutIndex === layouts.length - 1 ? 0 : newLayoutIndex + 1;
